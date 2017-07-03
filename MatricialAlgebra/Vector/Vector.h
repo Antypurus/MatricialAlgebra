c04d88f5801152d76225e9723990238039302d8a
Tiago Magalhães <up201607931@fe.up.pt>
2017-07-03 01:51:16 +0100

    update

--- MatricialAlgebra/Vector/Vector.h
+++ MatricialAlgebra/Vector/Vector.h
@@ -10,16 +10,30 @@ namespace MatricialAlgebra {
 		bool m_isNULL = false;
 	public:
 		/*
-		Standard Constructor For the Vector , all values will be instanced as 0
-		and the number of values to the vector is passed as an argument
+		Standard Constructor For the Vector ,the number of values 
+		of the vector is passed as an argument
+
 		@param size - size of the mathematical vector
 		*/
 		Vector(size_t size);
 
 		/*
 		Returns the size of the vector object
+
 		@return - the number of values in the vector
 		*/
 		size_t getSize()const;
 	};
+
+	template<typename T>
+	inline Vector<T>::Vector(size_t size)
+	{
+		this->m_Vector = std::vector<T>(size);
+	}
+
+	template<typename T>
+	inline size_t Vector<T>::getSize() const
+	{
+		return this->m_Size;
+	}
 }
